6e6366a65e38cc00c086ce2535747ce10ca526c2
moodirmanooge-crypto <wlltahluuka@gmail.com>
2026-07-24 19:15:56 +0300

    new update

--- src/student/StudentIdCard.jsx
+++ src/student/StudentIdCard.jsx
@@ -21,7 +21,7 @@ import { db } from "../firebase/firebase";
 import schoolLogo from "../assets/rising-star-logo.png";
 
 const SCHOOL = {
-  name1: "RISING STAR",
+  name1:   "RISING STAR",
   name2: "PRIMARY & SECONDARY SCHOOL",
   tagline: "Education Is Life It Self",
   phone: "+252 61 2345678",
